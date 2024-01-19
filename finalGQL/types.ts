02e22752da0de66323e4c1422280b2dbcda09bbe
Alialdea <146476395+Alialdea@users.noreply.github.com>
2024-01-19 18:31:48 +0100

    Delete finalGQL directory

--- finalGQL/types.ts
+++
@@ -1,9 +0,0 @@
-export type Contacto={
-    nombre: string,
-    apellido1: string,
-    apellido2: string,
-    ntelefono: string, //hacer un validate de esto!!
-    paisres: string,
-    capital: string,
-    hora: string,
-}
